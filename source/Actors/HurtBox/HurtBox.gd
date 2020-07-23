1680c919aa17556d831e4878178e20a3185af0cb
Henrique Campos <henriiquecampos@gmail.com>
2020-07-23 18:57:58 -0300

    Add petty asteroid to level 1

--- source/Actors/HurtBox/HurtBox.gd
+++ source/Actors/HurtBox/HurtBox.gd
@@ -6,4 +6,4 @@ export var team := "enemy"
 
 func _on_area_entered(area: Area2D) -> void:
 	if not area.is_in_group(team):
-		emit_signal("damage_taken", area.damage) 
+		emit_signal("damage_taken", area.damage)
---
+++ source/Objects/Asteroid/Asteroid.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+export var speed := 300.0
+export var direction := Vector2.DOWN
+
+
+onready var _velocity := speed * direction
+
+onready var _health := $Health
+onready var _animator := $AnimationPlayer
+
+
+func _process(delta: float) -> void:
+	translate(_velocity * delta)
+
+
+func _on_Health_depleted() -> void:
+	_animator.play("explode")
+
+
+func _on_HurtBox_damage_taken(damage) -> void:
+	_health.current -= damage
+
