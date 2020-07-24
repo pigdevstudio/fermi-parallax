a3aae08231388a5f5e6b5c0434be07caf86dc1c5
The Honey Badger <32531175+Keyaki777@users.noreply.github.com>
2020-07-24 20:15:22 -0300

    add_enemy_health

--- source/Screens/PlayScreen.gd
+++ source/Screens/PlayScreen.gd
@@ -4,13 +4,14 @@ onready var enemy_healthbar := $HBoxContainer/Right/VBoxContainer/EnemyHealthBar
 onready var enemy_interface := $HBoxContainer/Right/VBoxContainer
 
 func _ready() -> void:
-	link_enemies_health()
+	get_tree().connect("node_added", self, "_on_SceneTree_node_added")
+#	link_enemies_health()
 
 
-func link_enemies_health() -> void:
-	for enemy in get_tree().get_nodes_in_group("enemy"):
-		enemy.connect("damaged", self, "update_enemy_healthbar")
-		enemy.connect("died", self, "_on_Enemy_died")
+#func link_enemies_health() -> void:
+#	for enemy in get_tree().get_nodes_in_group("enemy"):
+#		enemy.connect("damaged", self, "update_enemy_healthbar")
+#		enemy.connect("died", self, "_on_Enemy_died")
 
 
 func update_enemy_healthbar(health: ActionResource) -> void:
@@ -18,5 +19,11 @@ func update_enemy_healthbar(health: ActionResource) -> void:
 	enemy_interface.show()
 
 
-func _on_Enemy_died() -> void:
+func _on_Enemy_Health_depleted() -> void:
 	enemy_interface.hide()
+
+
+func _on_SceneTree_node_added(node_added: Node) -> void:
+	if node_added.is_in_group("enemy_health"):
+		update_enemy_healthbar(node_added)
+		node_added.connect("depleted", self, "_on_Enemy_Health_depleted")
