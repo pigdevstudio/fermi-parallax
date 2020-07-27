b91f72d498fd7b64570245f910cc36d6d6d26d5c
Henrique Campos <henriiquecampos@gmail.com>
2020-07-27 18:55:23 -0300

    Implement charging vfx

--- source/Actors/Resources/Replenish.gd
+++ source/Actors/Resources/Replenish.gd
@@ -1,5 +1,7 @@
 extends Timer
 
+signal started
+signal finished
 
 export var resource_path: NodePath
 export var recharge_per_second := 10.0
@@ -8,6 +10,7 @@ onready var resource: ActionResource = get_node_or_null(resource_path)
 
 func _ready() -> void:
 	set_process(false)
+	resource.connect("changed", self, "_on_Resource_changed")
 
 
 func _process(delta: float) -> void:
@@ -15,12 +18,14 @@ func _process(delta: float) -> void:
 	if resource.current >= resource.max_amount:
 		resource.depleted = false
 		set_process(false)
+		emit_signal("finished")
 
 
 func _on_timeout() -> void:
 	set_process(true)
+	emit_signal("started")
 
 
-func _on_Energy_changed(new_amount) -> void:
+func _on_Resource_changed(new_amount) -> void:
 	if not is_processing():
 		start()
