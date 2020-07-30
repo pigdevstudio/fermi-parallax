ade22b369af38075df0934a81a35fd7ec5d28ad8
Henrique Campos <henriiquecampos@gmail.com>
2020-07-30 18:49:40 -0300

    Fix player speed reseting during dash

--- source/Actors/Actions/Dash.gd
+++ source/Actors/Actions/Dash.gd
@@ -3,6 +3,8 @@ extends Action
 export var speed := 2000.0
 export var dash_duration := 0.5
 
+var _player_speed := 600.0
+
 onready var _dash_time := $DashTime
 
 
@@ -11,20 +13,26 @@ func _execute() -> void:
 		return
 	if player.direction.is_equal_approx(Vector2.ZERO):
 		return
-	if not _dash_time.is_stopped():
-		return
+	_player_speed = player.speed
+	player.speed = speed
 	player.velocity = player.direction * speed
 	_dash_time.start(dash_duration)
 	resource.current -= cost
 	player.animator.play("dash")
 
 
+func _reset_player_speed() -> void:
+	player.speed = _player_speed
+
+
 func cancel() -> void:
-	if not _dash_time.is_stopped():
-		player.update_velocity()
-		player.animator.play("cancel_dash")
+	if _dash_time.is_stopped():
+		return
+	_dash_time.stop()
+	_reset_player_speed()
+	player.animator.play("cancel_dash")
 
 
 func _on_DashTime_timeout() -> void:
-	player.update_velocity()
+	_reset_player_speed()
 	player.animator.play("cancel_dash")
