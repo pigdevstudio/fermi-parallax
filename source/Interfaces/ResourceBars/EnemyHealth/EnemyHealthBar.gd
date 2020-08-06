f33176b326a374b4676ec45af675daf588bcc2da
The Honey Badger <32531175+Keyaki777@users.noreply.github.com>
2020-08-06 19:56:27 -0300

    Update enemy healthBar

---
+++ source/Interfaces/ResourceBars/EnemyHealth/EnemyHealthBar.gd
@@ -0,0 +1,8 @@
+extends ResourceBar
+
+onready var animator = $AnimationPlayer
+
+func _on_Resource_value_changed(new_value: float) -> void:
+	if new_value < value:
+		animator.play("enemy_damaged")
+	._on_Resource_value_changed(new_value)
