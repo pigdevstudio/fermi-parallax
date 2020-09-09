3569ed26040839e0ade5aa2359dc8c730365c286
Henrique Campos <henriiquecampos@gmail.com>
2020-09-09 18:46:31 -0300

    Remove Spawner2D reference on Hurtbox

--- source/Objects/HurtBox/HurtBox.gd
+++ source/Objects/HurtBox/HurtBox.gd
@@ -4,8 +4,6 @@ signal damage_taken(damage)
 
 export var team := "enemy"
 
-onready var damage_label_spawner = $DamageLabelSpawner2D
-
 func _on_area_entered(area):
 	if not area.team == team:
 		emit_signal("damage_taken", area.damage)
